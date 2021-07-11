b10ad2e495f8002f1a0bc117f21379eacb195cce
Bwiza-Nina <ninamyles2020@gmail.com>
2021-07-11 14:36:34 +0200

    Dynamic demonstration of the navigation

--- JS/main.js
+++ JS/main.js
@@ -7,3 +7,17 @@ const navMenu = document.getElementById('nav-menu'),
               navMenu.classList.add('show-menu');
           })
       }
+
+      if(navClose){
+          navClose.addEventListener('click', ()=>{
+              navMenu.classList.remove('show-menu');
+          })
+      }
+
+      const navLink = document.querySelectorAll('nav-link');
+      function linkAction(){
+          const navMenu = document.getElementById('nav-menu');
+          // on each link we click remove the show-menu class //
+          navMenu.classList.remove('show-menu');
+      }
+      navLink.forEach(n => n.addEventListener('click', linkAction))
