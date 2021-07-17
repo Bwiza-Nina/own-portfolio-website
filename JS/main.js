0593d134c5e37aedf395eebd78e620c2419f1b8b
Bwiza-Nina <ninamyles2020@gmail.com>
2021-07-17 22:32:01 +0200

    Added the show header

--- JS/main.js
+++ JS/main.js
@@ -120,3 +120,17 @@ const navMenu = document.getElementById('nav-menu'),
             })
       }
       window.addEventListener('scroll', scrollActive)
+
+      function scrollHeader(){
+          const nav = document.getElementById('header')
+          if(this.scrollY >= 80)nav.classList.add('scroll-header');
+          else nav.classList.remove('scroll-header')
+      }
+      window.addEventListener('scroll', scrollHeader);
+
+      function scrollTop(){
+          const scrollTop = document.getElementById('scroll-top');
+          if(this.scrollY >= 560) scrollTop.classList.add('show-scroll');
+          else scrollTop.classList.remove('show-scroll')
+      }
+      window.addEventListener('scroll', scrollTop);
